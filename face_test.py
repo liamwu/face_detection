347ff5345d31de4d3fcd7fa1ee1569f6836f4403
Liam Wu <liam.wu@ubtrobot.com>
2019-04-20 21:23:01 +0800

    set input image and classifier in arguments

--- face_test.py
+++ face_test.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import argparse
 import matplotlib.pyplot as plt
 import time
 
@@ -12,22 +13,28 @@ def showImg(wname, img):
     cv.waitKey(0)
     cv.destroyAllWindows()
 
-def detectFaces(face_path):
+def detectFaces(face_path, calssifier):
     test = cv.imread(face_path)
     test_gray = convertToGRAY(test)
 
     #set cascade classifier
-    face_cascade = cv.CascadeClassifier("model\\haarcascade_frontalface_alt2.xml")
+    face_cascade = cv.CascadeClassifier(calssifier)
 
     #face detection function
-    faces = face_cascade.detectMultiScale(test_gray, scaleFactor=1.1, minNeighbors=5, minSize=(8, 8))
+    faces = face_cascade.detectMultiScale(test_gray, scaleFactor=1.08, minNeighbors=5, minSize=(8, 8))
     print(len(faces))
 
     #locate the face in original photo
     for(x, y, w, h) in faces:
-        cv.rectangle(test, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv.rectangle(test, (x, y), (x + w, y + h), (0, 255, 0), 1)
 
     showImg("out", test)
 
-face_path = "faces\\faces.jpg"
-detectFaces(face_path)
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--imagepath", required=True, help="set image path")
+ap.add_argument("-m", "--classifiermodel", required=True, help="set face calssifier model")
+args = vars(ap.parse_args())
+
+#face_path = "faces\\faces.jpg"
+#classifier = "model\\haarcascade_frontalface_alt2.xml"
+detectFaces(args["imagepath"], args["classifiermodel"])
